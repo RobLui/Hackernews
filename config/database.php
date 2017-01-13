6954a35d638870ed8a8a30079166467d21220d01
Robbert Luit <robbertluit@hotmail.com>
2017-01-13 13:15:09 +0100

    rm crud map = old version, rm welcome , index replaces this, add site to readme & database

--- config/database.php
+++ config/database.php
@@ -54,9 +54,9 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'robbertluit.mysql.eu2.frbit.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'opdracht'),
+            'database' => env('DB_DATABASE', 'robbertluit'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
