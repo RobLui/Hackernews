b9e8bddc64d2286fe5609f5a56ecdde03685a4d2
Robbert Luit <robbertluit@hotmail.com>
2017-01-21 00:46:45 +0100

    add votes model & votescontroller

---
+++ app/Http/Controllers/VotesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Comment;
+use App\Article;
+use App\Votes ;
+
+class VotesController extends Controller
+{
+  function voteHandler()
+  {
+
+  }
+}
---
+++ app/Votes.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Votes extends Model
+{
+  use SoftDeletes;
+  
+  protected $fillable = [
+      'up_down', 'voted_by', 'article_id',
+  ];
+}
